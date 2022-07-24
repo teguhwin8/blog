077f41b81b4edb35325b3841554497e791ace412
Teguh Widodo <teguhwin8@gmail.com>
2022-07-24 20:10:34 +0700

    excerpt moment

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,10 +1,6 @@
 import db from "../../../libs/db";
-
-export const config = {
-	api: {
-		responseLimit: false,
-	},
-};
+import excerptHtml from "excerpt-html";
+import moment from "moment";
 
 export default async function handler(req, res) {
 	if (req.method !== "GET") {
@@ -13,11 +9,45 @@ export default async function handler(req, res) {
 			.json({ message: `Method ${req.method} are not allowed` });
 	}
 
-	const posts = await db("posts").select(["id", "slug", "title", "content"]);
+	let currentPage = 1;
+	let perPage = 10;
+	let dataOffset = 0;
+
+	const { page, per_page } = req.query;
+
+	if (page && typeof parseInt(page) === "number" && parseInt(page) > 0) {
+		currentPage = parseInt(page);
+	}
+
+	if (
+		per_page &&
+		typeof parseInt(per_page) === "number" &&
+		parseInt(per_page) > 0
+	) {
+		perPage = parseInt(per_page);
+	}
+
+	dataOffset = (currentPage - 1) * perPage;
+
+	const posts = await db("posts")
+		.offset(dataOffset)
+		.limit(perPage)
+		.orderBy("updated_at", "desc");
+
+	moment.locale("id");
+	posts.map((post) => {
+		post.excerpt = excerptHtml(post.content);
+		post.created_at_from_now = moment(post.created_at).fromNow();
+		post.updated_at_from_now = moment(post.updated_at).fromNow();
+		post.created_at = moment(post.created_at).format("DD MMMM YYYY HH:mm");
+		post.updated_at = moment(post.updated_at).format("DD MMMM YYYY HH:mm");
+	});
 
 	return res.status(200).json({
 		message: "Success",
 		count: posts.length,
+		page: parseInt(currentPage),
+		perPage: parseInt(perPage),
 		data: posts,
 	});
 }
